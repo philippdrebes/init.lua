989fbb8f0a64d9627b11427b394193140c75822e
Philipp Drebes <philipp.drebes@gmail.com>
2024-04-07 18:26:45 +0200

    add numToStr/Comment.nvim

--- lua/pdrebes/lazy/colors.lua
+++ lua/pdrebes/lazy/colors.lua
@@ -1,15 +1,14 @@
 function ColorMyPencils()
-    color = color or "dracula_pro"
-    vim.cmd.colorscheme(color)
+	color = color or "dracula_pro"
+	vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-return
-{
-    dir = '~/.config/nvim/color/dracula_pro',
-    config = function()
-        ColorMyPencils()
-    end
+return {
+	dir = "~/.config/nvim/color/dracula_pro",
+	config = function()
+		ColorMyPencils()
+	end,
 }
