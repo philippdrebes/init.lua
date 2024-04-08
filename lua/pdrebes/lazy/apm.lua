19f7641d68b78fddb132e9fb3d5d8f64a95052cf
Philipp Drebes <philipp.drebes@softec.ch>
2024-04-08 08:02:43 +0200

    formatting

--- lua/pdrebes/lazy/apm.lua
+++ lua/pdrebes/lazy/apm.lua
@@ -1,9 +1,8 @@
 return {
     "ThePrimeagen/vim-apm",
-    config = function ()
+    config = function()
         local apm = require("vim-apm")
         apm:setup({})
         vim.keymap.set("n", "<leader>apm", function() apm:toggle_monitor() end)
-
     end
 }
