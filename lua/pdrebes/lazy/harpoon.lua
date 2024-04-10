c8e5e4074f8f531e8d8fc0ccf9a9e2472451e0fc
Philipp Drebes <philipp.drebes@softec.ch>
2024-04-10 09:04:37 +0200

    change harpoon bindings

--- lua/pdrebes/lazy/harpoon.lua
+++ lua/pdrebes/lazy/harpoon.lua
@@ -9,22 +9,12 @@ return {
         harpoon:setup()
         -- REQUIRED
 
-        --[[
-        settings = {
-            save_on_toggle = false,
-            sync_on_ui_close = true,
-            key = function()
-                return vim.loop.cwd()
-            end,
-        },
-        --]]
-
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(3) end)
         vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
         -- Toggle previous & next buffers stored within Harpoon list
