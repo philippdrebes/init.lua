8943b5e9be6898855fa05fc7cac2bfd4e2a4a671
Philipp Drebes <philipp.drebes@gmail.com>
2024-05-10 17:26:50 +0200

    remove unused comment

--- lua/pdrebes/lazy/treesitter.lua
+++ lua/pdrebes/lazy/treesitter.lua
@@ -33,8 +33,6 @@ return {
 				-- `false` will disable the whole extension
 				enable = true,
 
-				-- disable = { "latex" },
-
 				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 				-- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
 				-- Using this option may slow down your editor, and you may see some duplicate highlights.
