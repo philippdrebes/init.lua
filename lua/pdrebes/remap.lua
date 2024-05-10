1b9e600c9dfc961a4dbf6e14e1beb047f5b5d0eb
Philipp Drebes <philipp.drebes@gmail.com>
2024-05-10 17:34:36 +0200

    add remap for make

--- lua/pdrebes/remap.lua
+++ lua/pdrebes/remap.lua
@@ -31,6 +31,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+vim.keymap.set("n", "<leader>mk", "<cmd>!make<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
 	vim.cmd("so")
