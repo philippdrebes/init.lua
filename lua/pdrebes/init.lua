80323c7a8d6d627ce8a52e25649d10290b4fab37
Philipp Drebes <philipp.drebes@softec.ch>
2024-05-22 15:35:21 +0200

    remove leader f mapping from lsp conf

--- lua/pdrebes/init.lua
+++ lua/pdrebes/init.lua
@@ -25,9 +25,6 @@ autocmd('LspAttach', {
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     end
