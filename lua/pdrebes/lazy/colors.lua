e60951d676002a03441d66ca2c740742a010e9c0
Philipp Drebes <philipp.drebes@softec.ch>
2024-07-08 16:23:45 +0200

    use rose-pine

--- lua/pdrebes/lazy/colors.lua
+++ lua/pdrebes/lazy/colors.lua
@@ -1,23 +1,23 @@
 function ColorMyPencils()
-	-- color = color or "rose-pine"
-	color = color or "dracula_pro"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    -- color = color or "dracula_pro"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
+return {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+        ColorMyPencils()
+    end,
+}
+
 -- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
+-- 	dir = "~/.config/nvim/color/dracula_pro",
 -- 	config = function()
 -- 		ColorMyPencils()
 -- 	end,
 -- }
-
-return {
-	dir = "~/.config/nvim/color/dracula_pro",
-	config = function()
-		ColorMyPencils()
-	end,
-}
