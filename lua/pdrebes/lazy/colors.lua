c57446d7a2f19422a638a963450b77ad7e80d2a2
Philipp Drebes <philipp.drebes@gmail.com>
2024-07-10 18:34:17 +0200

    fix merge

--- lua/pdrebes/lazy/colors.lua
+++ lua/pdrebes/lazy/colors.lua
@@ -1,23 +1,23 @@
 function ColorMyPencils()
-    color = color or "rose-pine-moon"
-    -- color = color or "dracula_pro"
-    vim.cmd.colorscheme(color)
+	color = color or "rose-pine-moon"
+	-- color = color or "dracula_pro"
+	vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-        require("rose-pine").setup({
-            styles = {
-                transparency = true,
-            }
-        })
-        ColorMyPencils()
-    end,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparency = true,
+			},
+		})
+		ColorMyPencils()
+	end,
 }
 
 -- return {
@@ -27,9 +27,9 @@ return {
 -- 	end,
 -- }
 
-return {
-	dir = "~/.config/nvim/color/dracula_pro",
-	config = function()
-		ColorMyPencils()
-	end,
-}
+-- return {
+-- 	dir = "~/.config/nvim/color/dracula_pro",
+-- 	config = function()
+-- 		ColorMyPencils()
+-- 	end,
+-- }
