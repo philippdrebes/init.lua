6bbcafbf4bddad84ef021fa5ce8dfa0aa9079d02
Philipp Drebes <philipp.drebes@softec.ch>
2025-10-03 10:26:16 +0200

    disabled obsidian

--- lua/pdrebes/lazy/obsidian.lua
+++ lua/pdrebes/lazy/obsidian.lua
@@ -1,17 +1,17 @@
 return {
-	"epwalsh/obsidian.nvim",
-	version = "*",
-	lazy = true,
-	ft = "markdown",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	opts = {
-		workspaces = {
-			{
-				name = "default",
-				path = "~/vaults/default",
-			},
-		},
-	},
+	-- "epwalsh/obsidian.nvim",
+	-- version = "*",
+	-- lazy = true,
+	-- ft = "markdown",
+	-- dependencies = {
+	-- 	"nvim-lua/plenary.nvim",
+	-- },
+	-- opts = {
+	-- 	workspaces = {
+	-- 		{
+	-- 			name = "default",
+	-- 			path = "~/vaults/default",
+	-- 		},
+	-- 	},
+	-- },
 }
--- lua/pdrebes/set.lua
+++ lua/pdrebes/set.lua
@@ -32,4 +32,4 @@ vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
 
-vim.opt.conceallevel = 2
+-- vim.opt.conceallevel = 2
