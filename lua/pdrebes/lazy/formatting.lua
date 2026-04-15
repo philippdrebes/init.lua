9e2f7947dc1aec94b228fd697935cbd0a4017e0f
Philipp Drebes <philipp.drebes@softec.ch>
2026-04-15 13:30:56 +0200

    formatting

--- lua/pdrebes/lazy/formatting.lua
+++ lua/pdrebes/lazy/formatting.lua
@@ -1,55 +1,55 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	keys = {
-		{
-			"<leader>f",
-			function()
-				require("conform").format({ async = true, lsp_fallback = true })
-			end,
-			mode = "n",
-			desc = "Format buffer",
-		},
-	},
-	-- Everything in opts will be passed to setup()
-	opts = {
-		-- Define your formatters
-		formatters_by_ft = {
-			blade = { "blade-formatter", "rustywind" },
-			csharp = { "csharpier" },
-			css = { "prettier" },
-			graphql = { "prettier" },
-			html = { "prettier" },
-			htmldjango = { "prettier", "djlint" },
-			java = { "google-java-format" },
-			javascript = { "prettier" },
-			javascriptreact = { "prettier" },
-			json = { "prettier" },
-			lua = { "stylua" },
-			markdown = { "prettier" },
-			php = { "pint" },
-			python = { "isort", "yapf" },
-			sql = { "sql-formatter" },
-			svelte = { "prettier" },
-			tex = { "latexindent" },
-			typescript = { "prettier" },
-			typescriptreact = { "tailwindcss", "prettier" },
-			vue = { "tailwindcss", "prettier" },
-			xml = { "xmlformatter" },
-			yaml = { "yamlfmt", "prettier" },
-		},
-		-- Set up format-on-save
-		format_on_save = { timeout_ms = 800, lsp_fallback = true },
-		-- Customize formatters
-		-- formatters = {
-		-- 	shfmt = {
-		-- 		prepend_args = { "-i", "2" },
-		-- 	},
-		-- },
-	},
-	init = function()
-		-- If you want the formatexpr, here is the place to set it
-		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-	end,
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            "<leader>f",
+            function()
+                require("conform").format({ async = true, lsp_fallback = true })
+            end,
+            mode = "n",
+            desc = "Format buffer",
+        },
+    },
+    -- Everything in opts will be passed to setup()
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            blade = { "blade-formatter", "rustywind" },
+            csharp = { "csharpier" },
+            css = { "prettier" },
+            graphql = { "prettier" },
+            html = { "biome" },
+            htmldjango = { "prettier", "djlint" },
+            java = { "google-java-format" },
+            javascript = { "biome" },
+            javascriptreact = { "biome" },
+            json = { "prettier" },
+            lua = { "stylua" },
+            markdown = { "prettier" },
+            php = { "pint" },
+            python = { "isort", "yapf" },
+            -- sql = { "sql_formatter" },
+            svelte = { "biome" },
+            tex = { "latexindent" },
+            typescript = { "biome" },
+            typescriptreact = { "tailwindcss", "biome" },
+            vue = { "tailwindcss", "biome" },
+            xml = { "xmlformatter" },
+            yaml = { "yamlfmt", "prettier" },
+        },
+        -- Set up format-on-save
+        format_on_save = { timeout_ms = 800, lsp_fallback = true },
+        -- Customize formatters
+        -- formatters = {
+        --     shfmt = {
+        --         prepend_args = { "-i", "2" },
+        --     },
+        -- },
+    },
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
 }
