9f8c7f4d6a939b9bf7628fd7ade36dbda3ea313a
Philipp Drebes <philipp.drebes@softec.ch>
2026-06-25 12:40:15 +0200

    roslyn lsp config

--- lua/pdrebes/lazy/lsp.lua
+++ lua/pdrebes/lazy/lsp.lua
@@ -1,137 +1,152 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-		build = "make install_jsregexp",
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"j-hui/fidget.nvim",
-		},
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "j-hui/fidget.nvim",
+        },
 
-		opts = {
-			-- @type lspconfig.options
-			servers = {
-				intelephense = {
-					filetypes = { "php", "blade", "php_only" },
-					settings = {
-						intelephense = {
-							filetypes = { "php", "blade", "php_only" },
-							files = {
-								associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
-								maxSize = 5000000,
-							},
-						},
-					},
-				},
-			},
-		},
+        opts = {
+            -- @type lspconfig.options
+            servers = {
+                intelephense = {
+                    filetypes = { "php", "blade", "php_only" },
+                    settings = {
+                        intelephense = {
+                            filetypes = { "php", "blade", "php_only" },
+                            files = {
+                                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                                maxSize = 5000000,
+                            },
+                        },
+                    },
+                },
+            },
+        },
 
-		config = function()
-			local cmp = require("cmp")
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_lsp.default_capabilities()
-			)
+        config = function()
+            local cmp = require("cmp")
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities()
+            )
 
-			require("fidget").setup({})
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"rust_analyzer",
-				},
-				handlers = {
-					function(server_name) -- default handler (optional)
-						require("lspconfig")[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
+            require("fidget").setup({})
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "rust_analyzer",
+                },
+                handlers = {
+                    function(server_name) -- default handler (optional)
+                        require("lspconfig")[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
 
-					["lua_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup({
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									diagnostics = {
-										globals = { "vim", "it", "describe", "before_each", "after_each" },
-									},
-								},
-							},
-						})
-					end,
+                    ["lua_ls"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = {
+                                        globals = { "vim", "it", "describe", "before_each", "after_each" },
+                                    },
+                                },
+                            },
+                        })
+                    end,
 
-					["ts_ls"] = function()
-						local mason_registry = require("mason-registry")
-						local vue_language_server_path = mason_registry
-							.get_package("vue-language-server")
-							:get_install_path() .. "/node_modules/@vue/language-server"
+                    ["ts_ls"] = function()
+                        local mason_registry = require("mason-registry")
+                        local vue_language_server_path = mason_registry
+                            .get_package("vue-language-server")
+                            :get_install_path() .. "/node_modules/@vue/language-server"
 
-						local lspconfig = require("lspconfig")
+                        local lspconfig = require("lspconfig")
 
-						lspconfig.ts_ls.setup({
-							init_options = {
-								plugins = {
-									{
-										name = "@vue/typescript-plugin",
-										location = vue_language_server_path,
-										languages = { "vue" },
-									},
-								},
-							},
-							filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-						})
-					end,
-				},
-			})
+                        lspconfig.ts_ls.setup({
+                            init_options = {
+                                plugins = {
+                                    {
+                                        name = "@vue/typescript-plugin",
+                                        location = vue_language_server_path,
+                                        languages = { "vue" },
+                                    },
+                                },
+                            },
+                            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+                        })
+                    end,
+                },
+            })
 
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            vim.lsp.config("roslyn_ls", {
+                capabilities = {
+                    workspace = {
+                        didChangeWatchedFiles = {
+                            dynamicRegistration = true,
+                        },
+                    },
+                },
+                cmd = {
+                    'roslyn-language-server',
+                    '--stdio',
+                    '--autoLoadProjects',
+                },
+            })
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<C-y>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-			vim.diagnostic.config({
-				-- update_in_insert = true,
-				float = {
-					focusable = false,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			})
-		end,
-	},
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" }, -- For luasnip users.
+                }, {
+                    { name = "buffer" },
+                }),
+            })
+
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            })
+        end,
+    },
 }
